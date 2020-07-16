12dfee202c8455dcdb693584b72df468b94ade4a
skku-sungjin <skku7714@gmail.com>
2020-07-16 23:33:51 +0900

    functional buttons

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
-  return <div className="App">quiz</div>;
-}
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
